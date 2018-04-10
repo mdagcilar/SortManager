3ea7ac24d495f38ef80e92ce2147242cb5bddc4d
Metin Dagcilar <md3g11@soton.ac.uk>
2018-04-10 21:52:05 +0100

    small change

---
+++ src/main/java/com/m3c/md/model/Constants.java
@@ -0,0 +1,16 @@
+package com.m3c.md.model;
+
+/**
+ * Class of constants
+ *
+ * @author Metin Dagcilar
+ * @version 1.0
+ * @since 2018-04-03
+ */
+
+public class Constants {
+    public static final String TYPE_NOT_FOUND = "Sorry - this sorter type is not available";
+    public static final String CONFIG_ERROR = "Sorry - Configuration file not found";
+    public static final String FACTORY_PROPERTIES_FILE = "src/main/resources/factory.properties";
+    public static final String LOG_PROPERTIES_FILE = "src/main/resources/log4j.properties";
+}
