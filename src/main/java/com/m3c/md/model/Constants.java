9718c4a88042e872c681bc2a2a772f8298cce379
mdagcilar <md3g11@soton.ac.uk>
2018-04-11 13:22:45 +0100

    final changes to resource folder location

--- src/main/java/com/m3c/md/model/Constants.java
+++ src/main/java/com/m3c/md/model/Constants.java
@@ -11,6 +11,6 @@ package com.m3c.md.model;
 public class Constants {
     public static final String TYPE_NOT_FOUND = "Sorry - this sorter type is not available";
     public static final String CONFIG_ERROR = "Sorry - Configuration file not found";
-    public static final String FACTORY_PROPERTIES_FILE = "src/main/resources/factory.properties";
-    public static final String LOG_PROPERTIES_FILE = "src/main/resources/log4j.properties";
+    public static final String FACTORY_PROPERTIES_FILE = "resources/factory.properties";
+    public static final String LOG_PROPERTIES_FILE = "resources/log4j.properties";
 }
